501faa9ac5ddd7647cebce98a24e7bf1b0441a3d
BHARGAV <BHARGAV@BHARGAV-PC>
2019-09-25 13:49:47 +0530

    deleted model and employee bean class and adding maven complier in pom

--- springb3/src/main/java/springbootproj2/DemoApplication.java
+++ springb3/src/main/java/springbootproj2/DemoApplication.java
@@ -2,22 +2,24 @@ package springbootproj2;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+
 @RestController
 public class DemoApplication {
-   @Value("${spring.application.name}")
-   private String name;
-   public static void main(String[] args) {
-      SpringApplication.run(DemoApplication.class, args);
-   }
-   @RequestMapping(value = "/")
-   public String name() {
-      return name;
-   }
-}   
+	@Value("${spring.application.name}")
+	private String name;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RequestMapping(value = "/")
+	public String name() {
+		return name;
+	}
+}
